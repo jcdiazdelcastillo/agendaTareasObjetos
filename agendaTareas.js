71a4b50b0e9b8a1297534682d1c0e6b7ba06318f
jcdiazdelcastillo <147615359+jcdiazdelcastillo@users.noreply.github.com>
2025-10-02 19:26:38 +0200

    Add files via upload

---
+++ agendaTareas.js
@@ -0,0 +1,140 @@
+"use strict"
+
+const agenda = {
+    nombre:"",
+    tareas:[],
+
+    agregarTarea(descrip, priorid){
+        const tarea = {
+            descripcion:descrip,
+            prioridad:priorid,
+            estado:"pendiente"
+        }
+        this.tareas.push(tarea);
+    },
+
+    listarTareas(){
+        if(this.tareas.length==0)
+            console.log("No hay tareas");
+        else{
+            this.tareas.forEach ((tarea, i)=>{
+            console.log("TAREA" + i + " Descricpion: "+tarea.descripcion +" Priodidad: "+ tarea.prioridad + " Estado: " + tarea.estado);
+        });
+    }
+
+    },
+
+    marcarCompletada(indice){
+        if(indice<0 || indice>this.tareas.length){
+            console.log("INDICE INVÁLIDO");
+        }else{
+            this.tareas[indice].estado = "completada";
+        }
+
+    },
+
+    eliminarTarea(indice){
+        if(indice<0 || indice>this.tareas.length){
+            console.log("INDICE INVÁLIDO");
+        }else{
+            this.tareas.splice(indice, 1); //splice metodo predefinido de js. se le pasa por parámetos el índice q se quiere borrar y el número de elementos a borrar a partir de ahí
+        }
+    },
+
+    listarPendientes(){
+        console.log("TAREAS PENDIENTES");
+        this.tareas.forEach((tarea, i)=>{
+            if(tarea.estado == "pendiente"){
+                console.log("Tarea "+i+": "+tarea.descripcion+"\n");
+            }
+        });
+    },
+
+    ordenarPorPrioridad(){
+        for(let i=0; i<this.tareas.length-1; i++){
+            for(let j=0; j<this.tareas.length-i-1; j++){
+                if(this.tareas[j].prioridad>this.tareas[j+1].prioridad){
+                    let aux=this.tareas[j];
+                    this.tareas[j]=this.tareas[j+1];
+                    this.tareas[j+1]=aux;
+                }
+            }
+        }
+    },
+
+    //mia
+    visualizarAgenda(){
+        console.log("AGENDA DE: "+this.nombre+"\n");
+        this.tareas.forEach((tarea,i)=>{
+            console.log("TAREA" + i + " Descricpion: "+tarea.descripcion +" Priodidad: "+ tarea.prioridad + " Estado: " + tarea.estado);
+        });
+    }
+}
+
+agenda.nombre=prompt("Introduce dueño de agenda (NOMBRE): ");
+
+menuParte1();
+
+function menuParte1() {
+    let op;
+    do {
+        console.log("\nIntroduce opcion:");
+        console.log("1 - Agregar tarea");
+        console.log("2 - Listar Tareas");
+        console.log("3 - Marcar como completada");
+        console.log("4 - Eliminar tarea");
+        console.log("5 - Listar tareas pendientes");
+        console.log("6 - Ordenar por prioridad");
+        console.log("7 - Visualizar agenda");
+        console.log("8 - Salir");
+
+        op = Number(prompt("Introduce opcion: "));
+        menuParte2(op);
+
+    } while(op != 8);
+}
+
+function menuParte2(op) {
+
+    switch(op) {
+        case 1:
+            agregarTareaFuera();
+            break;
+        case 2:
+            agenda.listarTareas();
+            break;
+        case 3:
+            let indice = Number(prompt("Introduce tarea para marcar como completada: "));
+            agenda.marcarCompletada(indice);
+            break;
+        case 4:
+            let ind = Number(prompt("Introduce tarea a eliminar: "));
+            agenda.eliminarTarea(ind);
+            break;
+        case 5:
+            agenda.listarPendientes();
+            break;
+        case 6:
+            agenda.ordenarPorPrioridad();
+            break;
+        case 7:
+            agenda.visualizarAgenda();
+            break;
+        case 8:
+            console.log("SALIENDO...");
+            break;
+        default:
+            console.log("NO VALIDO");
+    }
+
+}
+function agregarTareaFuera(){
+    let prioridad;
+    let descripcion=prompt("Introduce descripción: ");
+    do{
+        prioridad = Number(prompt("introduce prioridad (entre 1-3): "));
+    }while(prioridad <1 || prioridad>3);
+    
+    agenda.agregarTarea(descripcion, prioridad);
+
+}
